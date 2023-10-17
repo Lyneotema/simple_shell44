d222d19ad1158112d1fc333aecbaf7b11a0bb4e9
Albert <chrismitto9@gmail.com>
2023-10-17 08:11:45 -0700

    2-builtin.c

---
+++ 2-builtin.c
@@ -0,0 +1,97 @@
+#include "shell.h"
+
+/**
+ * my_exit - Handles the "exit" command in the shell
+ * @info: A pointer to the structure containing command arguments
+ *
+ * Return: Returns 0 if successful, 1 on error, and -2 to indicate exit.
+ */
+int my_exit(info_t *info)
+{
+	int exit_status;
+
+	if (info->argc > 1) /* If there is an argument after "exit" */
+	{
+		exit_status = _err_atoi(info->argv[1]);
+		if (exit_status == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = exit_status;
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * my_cd - Changes the current working directory
+ * @info: A pointer to the structure containing command arguments
+ *
+ * Return: Always returns 0
+ */
+int my_cd(info_t *info)
+{
+	char *current_dir, *new_dir, buffer[1024];
+	int chdir_ret;
+
+	current_dir = getcwd(buffer, 1024);
+	if (!current_dir)
+		_puts("TODO: >>getcwd failure message here<<\n");
+	if (!info->argv[1])
+	{
+		new_dir = _getenv(info, "HOME=");
+		if (!new_dir)
+			chdir_ret = chdir((new_dir = _getenv(info, "PWD=")) ? new_dir : "/");
+		else
+			chdir_ret = chdir(new_dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(current_dir);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD="));
+		_putchar('\n');
+		chdir_ret = chdir((new_dir = _getenv(info, "OLDPWD=")) ? new_dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]);
+		_eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * my_help - Displays a help message for the shell
+ * @info: A pointer to the structure containing command arguments
+ *
+ * Return: Always returns 0
+ */
+int my_help(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("Help command is not yet implemented.\n");
+	if (0)
+		_puts(*arg_array); /* Temporary unused workaround */
+	return (0);
+}
+
