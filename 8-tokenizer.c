a40b8b79f299647dffec297421325da087304cda
AlbertMitto <94157652+Albert405C@users.noreply.github.com>
2023-10-17 21:02:37 +0300

    8-tokenizer.c

--- 8-tokenizer.c
+++ 8-tokenizer.c
@@ -1,57 +1,58 @@
-
 #include "shell.h"
 
 /**
  * strtow - Split a string into words using specified delimiters.
  * @str: Input string to split.
- * @d: Delimiter string.
+ * @delimiters: Delimiter string.
+ *
  * Return: Array of strings on success, or NULL on failure.
  */
-char **strtow(char *str, char *d)
+char **strtow(char *str, char *delimiters)
 {
-    int i, j, k, m, numwords = 0;
-    char **s;
+    int i, j, k, m, num_words = 0;
+    char **words;
 
     if (str == NULL || str[0] == '\0')
         return (NULL);
 
-    if (!d)
-        d = " ";
+    if (!delimiters)
+        delimiters = " ";
 
     for (i = 0; str[i] != '\0'; i++)
-        if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || str[i + 1] == '\0'))
-            numwords++;
+        if (!is_delimiter(str[i], delimiters) && (is_delimiter(str[i + 1], delimiters) || str[i + 1] == '\0'))
+            num_words++;
 
-    if (numwords == 0)
+    if (num_words == 0)
         return (NULL);
 
-    s = malloc((numwords + 1) * sizeof(char *));
-    if (!s)
+    words = malloc((num_words + 1) * sizeof(char *));
+    if (!words)
         return (NULL);
 
-    for (i = 0, j = 0; j < numwords; j++)
+    for (i = 0, j = 0; j < num_words; j++)
     {
-        while (is_delim(str[i], d))
+        while (is_delimiter(str[i], delimiters))
             i++;
 
         k = 0;
-        while (!is_delim(str[i + k], d) && str[i + k])
+        while (!is_delimiter(str[i + k], delimiters) && str[i + k])
             k++;
 
-        s[j] = malloc((k + 1) * sizeof(char));
-        if (!s[j])
+        words[j] = malloc((k + 1) * sizeof(char));
+        if (!words[j])
         {
             for (k = 0; k < j; k++)
-                free(s[k]);
-            free(s);
+                free(words[k]);
+            free(words);
             return (NULL);
         }
 
         for (m = 0; m < k; m++)
-            s[j][m] = str[i++];
-        s[j][m] = '\0';
+            words[j][m] = str[i++];
+        words[j][m] = '\0';
     }
 
-    s[j] = NULL;
-    return (s);
+    words[j] = NULL;
+    return (words);
 }
+
