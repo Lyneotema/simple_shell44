85aaffc54dcae633dc3cb4cde16380d0d69aafe2
AlbertMitto <94157652+Albert405C@users.noreply.github.com>
2023-10-19 00:08:13 +0300

    betty.c

--- betty.c
+++ betty.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * get_input - Read a line of input from the user.
  * @info: A pointer to the info_t structure.
@@ -8,10 +7,10 @@
  */
 ssize_t get_input(info_t *info)
 {
-    ssize_t nread;
+        ssize_t nread;
 
-    nread = _getline(info, &info->arg, NULL);
-    return (nread);
+        nread = _getline(info, &info->arg, NULL);
+        return (nread);
 }
 
 /**
@@ -20,8 +19,8 @@ ssize_t get_input(info_t *info)
  */
 void execute_command(info_t *info)
 {
-    /* Implement command execution logic here */
-    /* You will need to handle creating child processes */
+        /* Implement command execution logic here */
+        /* You will need to handle creating child processes */
 }
 
 /**
@@ -30,25 +29,25 @@ void execute_command(info_t *info)
  */
 int main(void)
 {
-    info_t info = INFO_INIT;
-    ssize_t nread;
+        info_t info = INFO_INIT;
+        ssize_t nread;
 
-    do {
-        if (isatty(STDIN_FILENO)) {
-            _puts("($) ");
-        }
-        nread = get_input(&info);
-        if (nread == -1) {
-            _puts("Error reading input\n");
-            break;
-        }
-        if (nread > 1) {
-            remove_comments(info.arg);
-            execute_command(&info);
-        }
-    } while (nread != -1);
+        do {
+                if (isatty(STDIN_FILENO)) {
+                        _puts("($) ");
+                }
+                nread = get_input(&info);
+                if (nread == -1) {
+                        _puts("Error reading input\n");
+                        break;
+                }
+                if (nread > 1) {
+                        remove_comments(info.arg);
+                        execute_command(&info);
+                }
+        } while (nread != -1);
 
-    free_info(&info, 0);
-    return (0);
+        free_info(&info, 0);
+        return (0);
 }
 
