82898c7119dffb652125a10f0d620d6ba106e71c
AlbertMitto <94157652+Albert405C@users.noreply.github.com>
2023-10-19 00:13:55 +0300

    betty.c

--- betty.c
+++ betty.c
@@ -1,16 +1,21 @@
 #include "shell.h"
+
 /**
  * get_input - Read a line of input from the user.
  * @info: A pointer to the info_t structure.
  *
- * Return: The number of characters read.
+ * Return: The number of characters read, or -1 on failure.
  */
 ssize_t get_input(info_t *info)
 {
-        ssize_t nread;
+    ssize_t nread;
 
-        nread = _getline(info, &info->arg, NULL);
-        return (nread);
+    nread = getline(&info->arg, &info->arg_len, stdin);
+    if (nread == -1) {
+        perror("Error reading input");
+    }
+
+    return nread;
 }
 
 /**
@@ -19,8 +24,22 @@ ssize_t get_input(info_t *info)
  */
 void execute_command(info_t *info)
 {
-        /* Implement command execution logic here */
-        /* You will need to handle creating child processes */
+    /* Implement command execution logic here */
+    /* You will need to handle creating child processes */
+    // Example:
+    // pid_t child_pid = fork();
+    // if (child_pid == -1) {
+    //     perror("Fork failed");
+    //     exit(EXIT_FAILURE);
+    // } else if (child_pid == 0) {
+    //     // Child process code
+    //     // Execute the command using execvp, for example
+    //     // Example: execvp(info->arg[0], info->arg);
+    //     perror("Command execution failed");
+    //     exit(EXIT_FAILURE);
+    // } else {
+    //     // Parent process code (you may need to wait for the child)
+    // }
 }
 
 /**
@@ -29,25 +48,30 @@ void execute_command(info_t *info)
  */
 int main(void)
 {
-        info_t info = INFO_INIT;
-        ssize_t nread;
+    info_t info = INFO_INIT;
+    ssize_t nread;
 
-        do {
-                if (isatty(STDIN_FILENO)) {
-                        _puts("($) ");
-                }
-                nread = get_input(&info);
-                if (nread == -1) {
-                        _puts("Error reading input\n");
-                        break;
-                }
-                if (nread > 1) {
-                        remove_comments(info.arg);
-                        execute_command(&info);
-                }
-        } while (nread != -1);
+    do {
+        if (isatty(STDIN_FILENO)) {
+            _puts("($) ");
+        }
+        nread = get_input(&info);
+        if (nread == -1) {
+            _puts("Error reading input\n");
+            break;
+        }
+        if (nread > 1) {
+            // Remove trailing newline character
+            if (info.arg[nread - 1] == '\n') {
+                info.arg[nread - 1] = '\0';
+            }
+            // You may want to tokenize the input for command handling
+            execute_command(&info);
+        }
+    } while (nread != -1);
 
-        free_info(&info, 0);
-        return (0);
+    free_info(&info, 0);
+    return (0);
 }
+
 
